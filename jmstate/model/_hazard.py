Be more careful about broadcasting to make sure no bug is happening

jmstate/model/_hazard.py
@@ -99,13 +99,17 @@ class HazardMixin:
         base = self.params.base_hazards[str_key](t0, t1)
 
         # Compute time-varying effects
-        mod = (
-            self.design.link_fns[key](t1, indiv_params)
-            @ self.params.link_coefs[str_key]
-        )
+        mod = 0
+        if str_key in self.params.link_coefs:
+            mod = (
+                self.design.link_fns[key](t1, indiv_params)
+                @ self.params.link_coefs[str_key]
+            )
 
         # Compute covariates effect
-        var = x @ self.params.x_coefs[str_key].unsqueeze(-1)
+        var = 0
+        if str_key in self.params.x_coefs:
+            var = x @ self.params.x_coefs[str_key].unsqueeze(-1)
 
         return base + mod + var
 
@@ -129,9 +133,11 @@ class HazardMixin:
         Returns:
             torch.Tensor: The computed cumulative hazard.
         """
+        # Careful with negative times
+        t1 = torch.max(t0, t1)
+
         # Transform to quadrature interval
         half = 0.5 * (t1 - t0)
-
         quad = (
             0.5 * (t0 + t1).unsqueeze(-1) + half.unsqueeze(-1) * self._std_nodes
         ).flatten(start_dim=-2)
@@ -219,7 +225,7 @@ class HazardMixin:
             with rows corresponding to individuals and columns to prediction times.
         """
         assert_all_finite(u, input_name="u")
-        check_consistent_length(u, sample_data)
+        u = torch.broadcast_to(u, (len(sample_data), -1))
 
         x = sample_data.x
         indiv_params = sample_data.indiv_params

jmstate/model/_predict.py
@@ -217,7 +217,7 @@ class PredictMixin(HazardMixin, MCMCMixin):
                 `(n_samples, n, m)`, stacked along the first dimension.
         """
         assert_all_finite(u, input_name="u")
-        check_consistent_length(u, data)
+        u = torch.broadcast_to(u, (len(data), -1))
 
         # Load and complete data
         data = ModelDataUnchecked(

jmstate/types/_data.py
@@ -213,11 +213,11 @@ class ModelData(BaseEstimator):
         assert_all_finite(self.c, input_name="c")
 
         check_consistent_length(self.x, self.y, self.c, self.trajectories)
-        check_consistent_length(self.t.transpose(0, -1), self.y.transpose(0, -2))
+        torch.broadcast_to(self.t, self.y.shape[:-1])
 
         # Check NaNs between t and y
         if ((~self.y.isnan()).any(dim=-1) & self.t.isnan()).any():
-            raise ValueError("NaN time values on non NaN y values")
+            raise ValueError("NaN time values on non NaN y values are not allowed")
 
 
 @dataclass