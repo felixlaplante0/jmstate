23c84ffc8ea2b23d210fc51c582668317d79c321
Félix Laplante <75139846+felixlaplante0@users.noreply.github.com>
2026-03-21 21:19:30 +0100

    Delete diagrams directory

--- diagrams/render.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-from pathlib import Path
-
-for qmd_file in Path(__file__).parent.glob("*.qmd"):
-    subprocess.run(["quarto", "render", str(qmd_file), "--to", "pdf"], check=True)
